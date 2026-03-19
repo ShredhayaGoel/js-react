431b7a73814e75a200833dc33bc2bb18c45b57b1
Shredhaya <shredhayagoel@gmail.com>
2026-03-19 23:40:06 +0530

    destructuring the objects

---
+++ SectionA/objectdestructing.js
@@ -0,0 +1,7 @@
+const course = {
+    course: 'javascript + react',
+    price: 5999,
+    courseInstructor: 'HiteshChaudhary'
+}
+const { courseInstructor: instructor } = course
+console.log(instructor)  
