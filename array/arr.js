833c09adc6d8121bba0e082c86ce83fa549e7da9
Shredhaya <shredhayagoel@gmail.com>
2026-03-29 23:57:32 +0530

    arrays methods

---
+++ array/arr.js
@@ -0,0 +1,26 @@
+// ARRAYS METHODS AND FUNCTOINS
+
+const a1 = [1, 2, 3, 'a', true, false];
+console.log(a1);
+a1.push(5);
+console.log(a1);
+a1.pop(5);
+console.log(a1);
+a1.unshift(2)
+
+console.log(a1);
+a1.shift(2)
+console.log(a1);
+console.log("join");
+
+console.log(a1.join());
+console.log(a1.includes(5));
+console.log(a1.indexOf(3));
+
+console.log(typeof a1.join());
+
+console.log("new type");
+
+const a2 = [7, 8, 9, 4, 5, 6, 1, 2, 30, true, false]
+a1.push(a2);
+console.log(a1);
